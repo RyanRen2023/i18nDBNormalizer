744596b00cbd44c001d5b812a8c432bc09c68c9e
RyanRen2023 <renxihai@gmail.com>
2024-11-05 12:58:07 -0500

    initial project

---
+++ src/main/java/com/algonquincollege/s4/cst8276/i18nDBNormalizer/I18nDbNormalizerApplication.java
@@ -0,0 +1,13 @@
+package com.algonquincollege.s4.cst8276.i18nDBNormalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I18nDbNormalizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I18nDbNormalizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algonquincollege/s4/cst8276/i18nDBNormalizer/I18nDbNormalizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algonquincollege.s4.cst8276.i18nDBNormalizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I18nDbNormalizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
